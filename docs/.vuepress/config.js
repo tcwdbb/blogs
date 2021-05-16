52096c9b242597d4c8093d1dbdb15c706e0b5cdb
程勇瑞 <chengyongrui@chengyongruideMacBook-Air.local>
2021-05-16 20:32:13 +0800

    blog

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,48 @@
+
+
+module.exports = {
+  title: "程勇瑞的个人博客",
+  description: '记录，让自己变的更好.',
+  dest: 'public',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+  ],
+  theme: 'reco',
+  themeConfig: {
+    type: 'blog',
+    // 博客设置
+    blogConfig: {
+      category: {
+        location: 2, // 在导航栏菜单中所占的位置，默认2
+        text: '分类', // 默认 “分类”
+      },
+      tag: {
+        location: 3, // 在导航栏菜单中所占的位置，默认3
+        text: 'Tag' // 默认 “标签”
+      },
+    },
+    nav: [
+      { text: 'Home', link: '/', icon: 'reco-home' },
+      { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' },
+    ],
+    // 搜索设置
+    search: true,
+    searchMaxSuggestions: 10,
+    // 自动形成侧边导航
+    sidebar: 'auto',
+    friendLink: [
+      {
+        title: '午后南杂',
+        desc: 'Enjoy when you can, and endure when you must.',
+        email: 'recoluan@qq.com',
+        link: 'https://www.recoluan.com'
+      },
+      // ...
+    ]
+  },
+  markdown: {
+    lineNumbers: true
+  },
+  plugins: ['@vuepress/medium-zoom', 'flowchart'],
+}  
---
+++ docs/views/test/test.md
@@ -0,0 +1,8 @@
+---
+title: 测试
+categories:
+ - 学习
+author: 123
+---
+
+1223
